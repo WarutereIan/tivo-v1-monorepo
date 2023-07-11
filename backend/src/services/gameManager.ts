2ceae5d1469d0150a915e2f04dc52addc9372be4
WarutereIan <nmwanik111@gmail.com>
2023-07-11 10:53:39 +0300

    turned off x-accel-buffering in sse res header

--- backend/src/services/gameManager.ts
+++ backend/src/services/gameManager.ts
@@ -89,6 +89,7 @@ export class PlayRound {
         "Content-Type": "text/event-stream",
         Connection: "keep-alive",
         "Cache-Control": "no-cache",
+        "X-Accel-Buffering": "no",
       };
 
       res.writeHead(200, headers);
