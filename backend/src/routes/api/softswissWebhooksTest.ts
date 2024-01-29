65ddbe0d2036c3e3baee73314d8bf0b745ab9e9c
WarutereIan <nmwanik111@gmail.com>
2024-01-29 13:30:49 +0000

    fixed sswiss rollback errors, added actions to queues

--- backend/src/routes/api/softswissWebhooksTest.ts
+++ backend/src/routes/api/softswissWebhooksTest.ts
@@ -1,18 +1,20 @@
 import { Router } from "express";
 import {
-  GCP_TO_WALLET_REQUESTS,
+  
+  GCP_play,
+  GCP_rollback,
   processSwissResult,
 } from "../../webhooks/softswiss";
 import { verifyRequestSignature } from "../../utils/softswiss/reqVerification";
 
 const router = Router();
 
-router.post("/play", verifyRequestSignature, GCP_TO_WALLET_REQUESTS.play);
+router.post("/play", verifyRequestSignature, GCP_play);
 
 router.post(
   "/rollback",
   verifyRequestSignature,
-  GCP_TO_WALLET_REQUESTS.rollbackRequest
+  GCP_rollback
 );
 
 module.exports = router;
